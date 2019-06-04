197615248acb6a6bc1d4d4f6812e612e913b8a4e
bassanodaniele <bassano.daniele@iisgalvanimi.edu.it>
2019-06-04 11:36:52 +0000

    fourth_commit

--- Front-end-app/src/app/rental-manager/rental-manager.component.ts
+++ Front-end-app/src/app/rental-manager/rental-manager.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-rental-manager',
   templateUrl: './rental-manager.component.html',
   styleUrls: ['./rental-manager.component.css']
 })
+
 export class RentalManagerComponent implements OnInit {
+
+  @Input() serverMonoRent: string;
+  @Input() user: User;
+
+  scooterInUse: boolean = false;
+  scooter: string = null;
+  public http:HttpClient;
+
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() { }
+
+
+  Start(scooter: HTMLInputElement): boolean {
+      this.http
+     .post(this.serverMonoRent + '/pick-up-scooter',
+       JSON.stringify({
+           username: this.user.username,
+           scooter: scooter.value
+       })
+      )
+      this.scooter = scooter.value;
+      return false;
+  }
+
+  Stop(): boolean {
+      this.http
+     .put(this.serverMonoRent + '/leave-scooter',
+       JSON.stringify({
+           username: this.user.username,
+           scooter: this.scooter
+       })
+      )
+      return false;
   }
 
 }
